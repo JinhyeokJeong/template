6e529201c77fcaab7fe754704ee72c6fe74f3c62
Jinhyeok Jeong <jjh00413@gmail.com>
2025-03-07 14:47:16 -0600

    build the first test package

--- R/hello.R
+++ R/hello.R
@@ -1,5 +1,37 @@
-
-## a placeholder
+#' @title Hello World!
+#' 
+#' @description This function will print out the words "Hello
+#' world!"
+#' 
+#' @param txt Character (length=1). 
+#' This argument will print the word input after the world, "Hello"
+#' 
+#' @author Jinhyeok Jeong <jjh00413@gmail.com>
+#' 
+#' @examples
+#' # Print the word "Hello, y'all"
+#' hello(txt = "y'all")
+#' 
+#' @export
+#' 
+# Hello function ----
+# Updated 03.07.2025
 hello <- function(txt = "world") {
     cat("Hello, ", txt, "\n")
 }
+
+#' @noRd 
+# Test function ----
+# Updated 03.07.2025
+test <- function(){
+  "This is a test"
+}
+
+#' @noRd
+# Tidyverse logo function ----
+# updated 03.07.2025
+logo <- function(){
+  tidyverse::tidyverse_logo()
+}
+
+  
